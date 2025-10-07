6ac31830c9ca7c214568b16edb6083786fe47812
Gabriel <gabrielms.castro@gmail.com>
2025-10-07 14:18:32 -0300

    feat: organizado credenciais de .env e caminhos de diretório em src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,32 @@
+import path from 'path'
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 dotenv.config();
 
 // AINDA ESTÁ MUITO RÍGIDO PARA QUE OUTRAS PESSOAS FORA DA MINHA REALIDADE UTILIZEM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const projectRoot = path.resolve(__dirname, '..');
+export const extensionDir = path.join(projectRoot, 'src', 'extensoes', 'presto_0.70'); // carregar de forma dinâmica futuramente
+export const userDataDir = path.join(projectRoot, '.temp', 'user-data');
+export const envs = loadEnv();
+
+type SystemCredentials = {
+  username: string;
+  password: string;
+}
 
 type Envs = {
   totpApiEndpoint: string;
   totpApiKey: string;
-
-  eprocUsuario1: string;
-  eprocSenha1: string;
-
-  esajUsuario1: string;
-  esajSenha1: string;
-
-  prestoUsuario1: string;
-  prestoSenha1: string;
+  sistemas : {
+    eproc?: SystemCredentials[];
+    esaj?: SystemCredentials[];
+    pje?: SystemCredentials[];
+    presto?: SystemCredentials[];
+    elaw?: SystemCredentials[];
+    projuris?: SystemCredentials[];
+  }
 };
 
 function requireEnv(name: string): string {
@@ -25,20 +37,35 @@ function requireEnv(name: string): string {
   return val;
 }
 
+function getEnv(name: string) :string | undefined {
+  return process.env[name];
+}
+
+function loadSystemCredentials(systemName: string): SystemCredentials[] {
+  const credentials: SystemCredentials[] = [];
+  let i = 1;
+  while (true) {
+    const username = getEnv(`${systemName}_USUARIO_${i}`);
+    const password = getEnv(`${systemName}_SENHA_${i}`);
+
+    if (!username || !password) break;
+
+    credentials.push({ username, password });
+    i++;
+  }
+  return credentials
+}
+
 function loadEnv(): Envs {
   return {
     totpApiEndpoint: requireEnv('TOTP_API_ENDPOINT'),
     totpApiKey: requireEnv('TOTP_API_KEY'),
-
-    eprocUsuario1: requireEnv('EPROC_USUARIO_1'),
-    eprocSenha1: requireEnv('EPROC_SENHA_1'),
-
-    esajUsuario1: requireEnv('ESAJ_USUARIO_1'),
-    esajSenha1: requireEnv('ESAJ_SENHA_1'),
-
-    prestoUsuario1: requireEnv('PRESTO_USUARIO_1'),
-    prestoSenha1: requireEnv('PRESTO_SENHA_1'),
+    sistemas: {
+      eproc: loadSystemCredentials('eproc'),
+      esaj: loadSystemCredentials('esaj'),
+      presto: loadSystemCredentials('presto'),
+    },
   };
 }
 
-export const envs = loadEnv();
+
